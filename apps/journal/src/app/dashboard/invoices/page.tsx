fa0f6a616ee29b40651215bf91fd26ca848e0848
wang xukun <wangxukun@hotmail.com>
2024-06-02 11:38:26 +0800

    Learn Next.js - Fetching Data

---
+++ apps/journal/src/app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable-next-line */
+export interface Props {}
+
+export default function index(props: Props) {
+  return (
+    <div>
+      <h1>hello invoices page</h1>
+    </div>
+  );
+}
---
+++ apps/learn-ts/src/main.ts
@@ -0,0 +1,5 @@
+console.log("Hello World, I'm learn-ts");
+
+function RootLayout({ children }: { children: 'abc' }) {
+  return 'bcd';
+}
